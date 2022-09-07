a2b02158d53b717845993710e2f83c183a06a0e8
Pedro Duarte <pduartesilva2005@gmail.com>
2022-09-07 18:29:20 -0300

    Desafio concluído :rocket: :purple_heart:

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-function openMenuMobile() {
-  const menuMobile = document.querySelector('#menu-mobile');
-  const buttonBurger = document.querySelector('#button-burger');
+const $menu = document.querySelector('[data-js="menu"]');
+const $navbar = document.querySelector('[data-js="navbar"]');
 
-  menuMobile.classList.toggle('active');
-  buttonBurger.classList.toggle('active');
+function openMenuMobile() {
+  const isOpen = $menu.classList.contains('open');
+
+  const classToAddInMenu = isOpen ? 'close' : 'open';
+  const classToRemoveInMenu = isOpen ? 'open' : 'close';
+
+  $navbar.classList.toggle('open');
+  $menu.classList.add(classToAddInMenu);
+  $menu.classList.remove(classToRemoveInMenu);
 }
+
+$menu.addEventListener('click', openMenuMobile);
